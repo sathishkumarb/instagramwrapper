c5275b58c88f48ec238881ad7c00c699b090a1f5
Sathish <meet.sathish@gmail.com>
2016-05-27 14:08:38 +0400

    php cs fix for psr2

--- InstagramClient.php
+++ InstagramClient.php
@@ -1,5 +1,6 @@
 <?php
-require('InstagramWrapper.php');
-$authcode = "63746d159de84b53bf50e872acd05691";
+
+require 'InstagramWrapper.php';
+$authcode = '63746d159de84b53bf50e872acd05691';
 $userdata = $instagramWr->userAuthToken($authcode);
 print_r($userdata);
