c7281b6cf64ba966fe40386a27557e6b04c4b046
Sathish <meet.sathish@gmail.com>
2016-05-27 11:26:29 +0400

    Pass test case for user authroize and redirect and generate code

--- InstagramTest.php
+++ InstagramTest.php
@@ -4,7 +4,8 @@
 // class warpper for instagram APi redirect authorization (access token) using ouath method and user details API
 */
 class InstagramWrapper {
-  // cosntants to connect and autorize api and error abbreviations
+
+  // constants to connect and autohrize api and error abbreviations
   const authorizationURL = 'https://api.instagram.com/oauth/authorize/';
   const authTokenURL = 'https://api.instagram.com/oauth/access_token';
   const csrfstate = 'csrfstate';
@@ -12,26 +13,30 @@ class InstagramWrapper {
   const errorReason = 'user_denied';
   const errorDescription = 'The user denied your request';
   const errorType = 'OAuthAccessTokenError';
+
   // scope limited variables to hold scope, clientid, secret code, authoirzatin code and redirect url
   private $clientId;
   private $clientSecret;
-  private $redirectURL = 'dev.tapetickets.com';
+  private $redirectURL = 'http://dev.tapetickets.com';
   private $authorizationCode;
   private $scopes = array('basic');
 
   public function __construct($settings){
-    // initialaizing clinet and secret
+      // initialaizing client , secret and redirect URL
       $this->clientId = $settings['cid'];
       $this->clientSecret = $settings['secret'];
-      $this->redirectURL = $settings['redirect'];
+      $this->redirectURL = (isset($settings['redirect']) && !empty($settings['redirect'])) ? $settings['redirect']:$this->redirectURL;
   }
-
+  
+  /* 
+  // @function handles the failand pass test cases and below are list of failover test cases on omission of any url params
+  // {"code": 400, "error_type": "OAuthException", "error_message": "Redirect URI does not match registered redirect URI"
+  // {"code": 400, "error_type": "OAuthException", "error_message": "Invalid scope field(s): basicstate=csrfstate"}
+  // {"code": 400, "error_type": "OAuthException", "error_message": "You must include a valid client_id, response_type, and redirect_uri parameters"}
+  // @params:$scope and state is optional by default scope has basic and can be extended to 'basic,likes' and state is csrf validation
+  */
   public function userRedirect($scope = null){
-    // return response handles failover and pass over test cases and below are list of failover test cases on omission of any url params
-    // {"code": 400, "error_type": "OAuthException", "error_message": "Redirect URI does not match registered redirect URI"
-    // {"code": 400, "error_type": "OAuthException", "error_message": "Invalid scope field(s): basicstate=csrfstate"}
-    // {"code": 400, "error_type": "OAuthException", "error_message": "You must include a valid client_id, response_type, and redirect_uri parameters"}
-    // $scope and state is optional by default scope has basic and can be extended to 'basic,likes' and state is csrf validation
+   
     if (isset($this->clientId) && !empty($this->clientId) && isset($this->redirectURL) && !empty($this->redirectURL)){
         return "https://api.instagram.com/oauth/authorize/?client_id=".$this->clientId."&redirect_uri=".$this->redirectURL.
         "&scope=".((isset($scope)&& !empty($scope)) ? $scope : implode("+",$this->scopes))."&state=".self::csrfstate.
@@ -40,15 +45,15 @@ class InstagramWrapper {
       // Handles invlid or null client id and secret mandatory to connect to initiate and connect API
       throw new Exception("Invalid Client Id and Secret Token Supplied");
     }
-    
+    return;
   }
 
 }
-
+// initialize more on read me
 $instagramWr = new InstagramWrapper(array(
-    'cid'      => '9d5d2b0944434b0d9ecc23b910fadde9',
-    'secret'   => 'bdb22bbe77434a668c78144c9af69520',
-    'redirect' => 'dev.tapetickets.com/public/'
+    'cid'      => 'b0940040034c49319c0e543fb94034da',
+    'secret'   => 'a96bc4b032544df88506f81b52597b1c',
+    'redirect' => 'http://dev.tapetickets.com/public/'
 ));
 echo $redirectTransfer = $instagramWr->userRedirect();
 echo "<a href='$redirectTransfer'>Login with Instagram</a>";
